9216eb4b25591d28a1e985be123f9528edb4c230
ArcturusSunrae <cb010914@students.apiit.lk>
2024-09-08 01:47:38 +0530

    supplier table in database.

---
+++ app/Models/Supplier.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'user_id',
+        'business_name',
+        'phone',
+        'location',
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
